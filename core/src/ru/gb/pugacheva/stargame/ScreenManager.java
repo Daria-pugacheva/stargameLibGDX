055ee2f3bdc44429b4137b2c08127c39ee0909cb
geekbrainsPugachevaD <dpugachev@uandex.ru>
2021-09-19 00:08:53 +0300

    Initial commit

---
+++ core/src/ru/gb/pugacheva/stargame/ScreenManager.java
@@ -0,0 +1,8 @@
+package ru.gb.pugacheva.stargame;
+
+import com.badlogic.gdx.Gdx;
+
+public class ScreenManager {
+    public static final int SCREEN_WIDTH = Gdx.graphics.getWidth();//960
+    public static final int SCREEN_HEIGHT = Gdx.graphics.getHeight();//540
+}
